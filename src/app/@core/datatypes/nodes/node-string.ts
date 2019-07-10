fdc83fbfc97e238d3c2e4b9eec881184742b7853
Töpfer, Philipp <Philipp.Toepfer@procom.de>
2019-07-10 13:20:53 +0200

    Add schema validators.

--- src/app/@core/datatypes/nodes/node-string.ts
+++ src/app/@core/datatypes/nodes/node-string.ts
@@ -10,11 +10,28 @@ export class NodeString extends Node implements IStringSchema {
 
 
   constructor(label: string, value: string, schema?: Partial<IStringSchema>) {
+    NodeString.validateSchema(schema);
     super(label, NodeType.STRING, schema);
     this.value = value;
     this.values = schema && schema.values || null;
     this.minimalLength = schema && schema.minimalLength || null;
     this.maximalLength = schema && schema.maximalLength || null;
+  }
+
+  private static validateSchema(schema: Partial<IStringSchema>): void {
+    if (!schema) {
+      return;
+    }
+    if (Object.keys(schema).includes('values') && !schema.values.every((value => typeof value === 'string'))) {
+      throw new Error('Values must be of type string');
+    }
+
+    if (Object.keys(schema).includes('minimalLength') && !(typeof schema.minimalLength === 'number')) {
+      throw new Error('MinimalLength must be of type number');
+    }
+    if (Object.keys(schema).includes('maximalLength') && !(typeof schema.maximalLength === 'number')) {
+      throw new Error('MaximalLength must be of type number');
+    }
   }
 
   isValid(): boolean {
