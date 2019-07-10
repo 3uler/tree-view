f636f08f5a29b80aaaa544b3e6ff0413119bc681
Töpfer, Philipp <Philipp.Toepfer@procom.de>
2019-07-10 13:21:52 +0200

    Remove angular flex layout.

--- src/app/components/tree/tree.module.ts
+++ src/app/components/tree/tree.module.ts
@@ -1,14 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { TreeComponent } from './tree.component';
-import { NodeBooleanComponent } from './nodes/node-boolean/node-boolean.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {TreeComponent} from './tree.component';
+import {NodeBooleanComponent} from './nodes/node-boolean/node-boolean.component';
 import {FormsModule} from '@angular/forms';
-import { NodeNumberComponent } from './nodes/node-number/node-number.component';
-import { NodeStringComponent } from './nodes/node-string/node-string.component';
-import { NodeObjectComponent } from './nodes/node-object/node-object.component';
-import { NodeListComponent } from './nodes/node-list/node-list.component';
-import {FlexModule} from '@angular/flex-layout';
-
+import {NodeNumberComponent} from './nodes/node-number/node-number.component';
+import {NodeStringComponent} from './nodes/node-string/node-string.component';
+import {NodeObjectComponent} from './nodes/node-object/node-object.component';
+import {NodeListComponent} from './nodes/node-list/node-list.component';
 
 
 @NgModule({
@@ -19,7 +17,6 @@ import {FlexModule} from '@angular/flex-layout';
   imports: [
     CommonModule,
     FormsModule,
-    FlexModule
   ]
 })
 export class TreeModule { }
